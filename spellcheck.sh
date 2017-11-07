ef00a55c22de826f26a1acda71ced2672035fe55
Paul Seyfert <pseyfert@cern.ch>
2017-11-07 12:30:28 +0100

    fix encoding in spellcheck

--- spellcheck.sh
+++ spellcheck.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo "checking file $1"
-if [[ $(cat $1 | aspell list -t -d en_GB -p ./.aspell.en.pws | wc -l) -eq 0 ]]
+if [[ $(cat $1 | aspell list --encoding=utf-8 -t -d en_GB -p ./.aspell.en.pws | wc -l) -eq 0 ]]
 then
   exit 0
 else
   echo "spellcheck of $1 failed due to:"
-  cat $1 | aspell list -t -d en_GB -p ./.aspell.en.pws | tee -a spelling-mistakes.txt
+  cat $1 | aspell list --encoding=utf-8 -t -d en_GB -p ./.aspell.en.pws | tee -a spelling-mistakes.txt
   exit 1
 fi
 
